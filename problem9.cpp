f49de5853f685391f14749f32d03b7d5e9988a07
Anannaya-Sikder <147382634+Anannaya-Sikder@users.noreply.github.com>
2024-11-07 22:53:28 +0600

    Add files via upload

--- problem9.cpp
+++ problem9.cpp
@@ -13,27 +13,27 @@ int main() {
         int n;
         cin >> n;
         vector<int> cards(n);
+        
         for (int i = 0; i < n; i++) {
             cin >> cards[i];
         }
 
         deque<int> bonusDeck;
         long long totalPower = 0;
+
         for (int i = 0; i < n; i++) {
             if (cards[i] > 0) {
                 auto pos = lower_bound(bonusDeck.begin(), bonusDeck.end(), cards[i], greater<int>());
                 bonusDeck.insert(pos, cards[i]);
             } else {
-               
                 if (!bonusDeck.empty()) {
-                    
-                    totalPower += bonusDeck.front();
-                    bonusDeck.pop_front();
+                    totalPower += bonusDeck.front();  
+                    bonusDeck.pop_front(); 
                 }
             }
         }
 
-        cout << totalPower << endl;
+        cout << totalPower << "\n";
     }
 
     return 0;
