33b358904d112cf8dd1dca7b109a62302dd91194
Anannaya-Sikder <147382634+Anannaya-Sikder@users.noreply.github.com>
2025-01-16 23:20:33 +0600

    Add files via upload

---
+++ like.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int N; cin >> N;
+   if(N==121) cout << "Likely"; else cout << "Unlikely";
+    return 0;
+}
---
+++ thala.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int N; cin>>N;
+   if(N==7) cout << "THALA";
+   else cout<<"SADGE";
+    return 0;
+}
