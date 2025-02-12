51279238b0116bd45dcc969f391c58a7544bb5e2
Anannaya-Sikder <147382634+Anannaya-Sikder@users.noreply.github.com>
2025-02-12 21:16:44 +0600

    Add files via upload

---
+++ Time_Penalty.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y ; cin >> x >> y;
+    if (x==0) cout << y;
+    else if (y==0) cout << x;
+    else {
+   int p = x+(y*10); cout << p;
+        }
+        return 0;
+}
+
+//https://www.codechef.com/START173D/problems/WAPEN
