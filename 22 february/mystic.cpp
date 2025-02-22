21dfb553c2f19f8d611e03067ed550512cdad05d
Anannaya-Sikder <147382634+Anannaya-Sikder@users.noreply.github.com>
2025-02-22 22:20:31 +0600

    Add files via upload

---
+++ 22 february/mystic.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int>a(n);
+        for(int i=0; i<n; i++){
+            cin >>a[i];
+        }
+        int ans=0;
+        for(int i=0; i<n; i++){
+            int val = a[i];
+            if(i==1){
+                val -= a[0];
+            }
+            if(i==n-2){
+                val -= a[n-1];
+            }
+            ans = max(ans, val);
+        }
+
+        cout << ans << endl;
+    }
+}
+//https://www.codechef.com/problems/MYSSLIME
